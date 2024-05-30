a53ffd9c7159bc8722e0df9f070586de795c1963
michaloumen <michelle.mendonca@minu.co>
2024-05-30 10:21:19 -0300

    adiciona .env

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@ import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
 import router from "./router";
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(
   cors({
@@ -25,10 +27,8 @@ server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
 });
 
-const MONGO_URL = "mongodb+srv://michellelmendonca:brvAhlONHJ99vVlu@cluster0.2rrhvvi.mongodb.net/restapi-node-express-typescript?retryWrites=true&w=majority&appName=Cluster0";
-
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log("error"));
 
 app.use("/", router());
